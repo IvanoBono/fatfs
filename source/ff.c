be5aad15a3020205d0ee201db72254314ad7e487
Ivano Bono <82436387+IvanoBono@users.noreply.github.com>
2022-01-11 09:42:53 +0100

    Update ff.c
    removed unnecessary pointer increment in function:
    dbc_2nd (BYTE c)

--- source/ff.c
+++ source/ff.c
@@ -816,7 +816,7 @@ static UINT put_utf (	/* Returns number of encoding units written (0:buffer over
 	}
 	if (szb < 2 || !IsSurrogateH(hs) || !IsSurrogateL(wc)) return 0;	/* Buffer overflow or wrong surrogate? */
 	*buf++ = hs;
-	*buf++ = wc;
+	*buf = wc;
 	return 2;
 
 #elif FF_LFN_UNICODE == 2	/* UTF-8 output */
@@ -830,14 +830,14 @@ static UINT put_utf (	/* Returns number of encoding units written (0:buffer over
 	if (chr < 0x800) {	/* 2-byte sequence? */
 		if (szb < 2) return 0;	/* Buffer overflow? */
 		*buf++ = (TCHAR)(0xC0 | (chr >> 6 & 0x1F));
-		*buf++ = (TCHAR)(0x80 | (chr >> 0 & 0x3F));
+		*buf = (TCHAR)(0x80 | (chr >> 0 & 0x3F));
 		return 2;
 	}
 	if (chr < 0x10000) {	/* 3-byte sequence? */
 		if (szb < 3 || IsSurrogate(chr)) return 0;	/* Buffer overflow or wrong code? */
 		*buf++ = (TCHAR)(0xE0 | (chr >> 12 & 0x0F));
 		*buf++ = (TCHAR)(0x80 | (chr >> 6 & 0x3F));
-		*buf++ = (TCHAR)(0x80 | (chr >> 0 & 0x3F));
+		*buf = (TCHAR)(0x80 | (chr >> 0 & 0x3F));
 		return 3;
 	}
 	/* 4-byte sequence */
@@ -849,7 +849,7 @@ static UINT put_utf (	/* Returns number of encoding units written (0:buffer over
 	*buf++ = (TCHAR)(0xF0 | (chr >> 18 & 0x07));
 	*buf++ = (TCHAR)(0x80 | (chr >> 12 & 0x3F));
 	*buf++ = (TCHAR)(0x80 | (chr >> 6 & 0x3F));
-	*buf++ = (TCHAR)(0x80 | (chr >> 0 & 0x3F));
+	*buf = (TCHAR)(0x80 | (chr >> 0 & 0x3F));
 	return 4;
 
 #elif FF_LFN_UNICODE == 3	/* UTF-32 output */
@@ -862,7 +862,7 @@ static UINT put_utf (	/* Returns number of encoding units written (0:buffer over
 		if (hc >= 0x100000 || chr >= 0x400) return 0;	/* Wrong surrogate? */
 		chr = (hc | chr) + 0x10000;
 	}
-	*buf++ = (TCHAR)chr;
+	*buf = (TCHAR)chr;
 	return 1;
 
 #else						/* ANSI/OEM output */
@@ -872,11 +872,11 @@ static UINT put_utf (	/* Returns number of encoding units written (0:buffer over
 	if (wc >= 0x100) {	/* Is this a DBC? */
 		if (szb < 2) return 0;
 		*buf++ = (char)(wc >> 8);	/* Store DBC 1st byte */
-		*buf++ = (TCHAR)wc;			/* Store DBC 2nd byte */
+		*buf = (TCHAR)wc;			/* Store DBC 2nd byte */
 		return 2;
 	}
 	if (wc == 0 || szb < 1) return 0;	/* Invalid char or buffer overflow? */
-	*buf++ = (TCHAR)wc;					/* Store the character */
+	*buf = (TCHAR)wc;					/* Store the character */
 	return 1;
 #endif
 }
